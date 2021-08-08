85559024a7e904b7ed2133bf07757e67372eb1e0
Neri Yarden <neriyarden@gmail.comgit add git add --allgit statusgit config --global user.email neriyarden@gmail.com>
2021-08-08 23:42:12 +0300

    signin error

--- routes/sign_in.py
+++ routes/sign_in.py
@@ -10,7 +10,13 @@ def sign_in():
     email = request.json['email']
     password = request.json['password']
     validated_user = User.validate_user(email, password)
-    print(str(validated_user.id))
+    print(validated_user)
+    if not validated_user:
+        return Response(
+            json.dumps({'error': 'Incorrect email or password'}),
+            status=404,
+            mimetype='application/json'
+        )
     resp = Response(validated_user.to_json(), status=200, mimetype='application/json')
     resp.set_cookie('session_id', str(validated_user.id))
     return resp
